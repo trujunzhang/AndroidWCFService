91db63e4e8d78dbb43acf1fc16b80f7afe539904
Daniel <swrhim@gmail.com>
2013-10-08 22:07:05 -0400

    First checkin

---
+++ AndroidService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using AndroidWCFService.Model;
+
+namespace AndroidWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class AndroidService : IAndroidService
+    {
+        public List<string> GetAllJokes()
+        {
+            List<string> Jokes = new List<string>();
+
+            using (AndroidEF ef = new AndroidEF())
+            {
+                Jokes = (from x in ef.Tables
+                         select x.Joke).ToList<string>();
+
+            }
+
+            return Jokes;
+        }
+
+        public bool AddJoke(string joke)
+        {
+            using (AndroidEF ef = new AndroidEF())
+            {
+                Table t = new Table();
+                t.Joke = joke;
+                ef.Tables.Add(t);
+                ef.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ IAndroidService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AndroidWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IAndroidService
+    {
+
+        [OperationContract]
+        List<string> GetAllJokes();
+
+        [OperationContract]
+        bool AddJoke(string joke);
+
+        // TODO: Add your service operations here
+    }
+
+}
